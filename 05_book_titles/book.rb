0cf512f7d93601c7ce4d41a5bc9b28be2f1e5ce0
Mark Genesis Sargento <genesistobiassargento@gmail.com>
2019-10-28 10:43:17 +0800

    Finish book titles exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
-# write your code here
+
+  def initialize
+    @title = ' '
+  end
+  def title
+    @title
+  end
+  def title=(title)
+    titles = title.split  ' '
+    i =0
+    titles.each do |t|
+      if t == 'the' or t =='an' or t =='a' or t == 'and' or t == 'of' or t =='in'
+        if i == 0
+          @title += t.capitalize.strip + ' '
+        else
+          @title += t.strip + ' '
+        end
+      else
+        @title += t.capitalize.strip + ' '
+      end
+      i += 1
+    end
+    @title = @title.strip
+  end
 end
